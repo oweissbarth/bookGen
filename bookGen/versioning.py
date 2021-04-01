46b2fa3972a6e3c6fca5520dd5d883a363715f2d
oweissbarth <mail@oweissbarth.de>
2021-04-01 19:47:09 +0200

    Look for bookGen collection not only in master collection.

--- bookGen/versioning.py
+++ bookGen/versioning.py
@@ -1,6 +1,7 @@
 """This file contains versioning functionality
 """
 import logging
+import bpy
 
 log = logging.getLogger("bookGen.versioning")
 
@@ -23,5 +24,7 @@ def version_upgrade_collection_pointer_property(scene):
         scene (bpy.types.Scene): the scene that should be upgraded
     """
     log.info("Performing version upgrade: collection_pointer_property on scene %s" % scene.name)
-    if "BookGen" in scene.collection.children.keys():
-        scene.BookGenAddonProperties.collection = scene.collection.children["BookGen"]
+    if "BookGen" in bpy.data.collections.keys():
+        scene.BookGenAddonProperties.collection = bpy.data.collections["BookGen"]
+    else:
+        log.error("Version upgrade failed. BookGen collection not found.")
